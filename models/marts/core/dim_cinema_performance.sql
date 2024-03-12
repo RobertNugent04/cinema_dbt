b36bb30b6111ecdb21cb90bf0f3dbbc62fc6ea55
Robert Nugent <robert.nugent@seaparkconsultancy.com>
2024-03-12 11:44:09 +0000

    Add cinema information fields to dim_cinema_performance model

--- models/marts/core/dim_cinema_performance.sql
+++ models/marts/core/dim_cinema_performance.sql
@@ -8,9 +8,15 @@ with transactions as(
     select * from {{ref('fct_transactions')}}
 ),
 
+cinemas as(
+    select * from {{ref('stg_cinemas')}}
+),
+
 cinema_performance as (
     select
         t.cinema_id,
+        c.cinema_name,
+        c.cinema_location,
         COUNT(DISTINCT t.movie_title) as num_movies_shown,
         COUNT(DISTINCT t.customer_id) as num_customers,
         SUM(t.tickets_bought) as total_tickets_sold,
@@ -18,7 +24,12 @@ cinema_performance as (
         MIN(t.transaction_date) as first_transaction_date,
         MAX(t.transaction_date) as last_transaction_date
     from transactions t
-    GROUP BY t.cinema_id
+    JOIN cinemas c ON t.cinema_id = c.cinema_id
+    GROUP BY 
+        t.cinema_id,
+        c.cinema_name,
+        c.cinema_location
+    
 )
 
 select * from cinema_performance
