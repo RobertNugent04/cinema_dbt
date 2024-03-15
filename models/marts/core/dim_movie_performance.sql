d3c55184afd69e9e1f95a240fa2ccbac88a6609f
Robert Nugent <robert.nugent@seaparkconsultancy.com>
2024-03-15 12:24:37 +0000

    Add genre and release_year fields to dim_movie_performance model

--- models/marts/core/dim_movie_performance.sql
+++ models/marts/core/dim_movie_performance.sql
@@ -8,14 +8,21 @@ with transactions as(
     select * from {{ref('fct_transactions')}}
 ),
 
+movies as(
+    select * from {{ref('stg_movies')}}
+),
+
 movie_performance as (
     select
         t.movie_title,
+        m.movie_genre,
+        m.release_year,
         SUM(t.tickets_bought) as tickets_sold,
         SUM(t.total) as revenue,
         RANK() OVER (ORDER BY revenue DESC) AS movie_ranking,
     from transactions t
-    GROUP BY t.movie_title
+    JOIN movies m ON m.movie_title = t.movie_title
+    GROUP BY t.movie_title, m.movie_genre, m.release_year
 )
 
 select * from movie_performance
